3f9bfe71914e798a640a3e9a397fbe59cd245645
“unaisprompt” <unais.vm@prompttechsolutions.com>
2023-03-23 15:36:16 +0530

    banner condition add

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,10 +9,12 @@
             <div id='rev_slider_4_wrapper' class='rev_slider_wrapper fullwidthbanner-container'>
               <div id='rev_slider_4' class='rev_slider fullwidthabanner'>
                 <ul>
+                  @if(!empty($finalData['main_data']['slider']))
                   @foreach($finalData['main_data']['slider'] as $banner)
+
                   <li data-transition='random' data-slotamount='7' data-masterspeed='1000'
-                    data-thumb='{{$banner['image']}}'>
-                    <img src='{{$banner['image']}}' data-bgfit='cover' data-bgrepeat='no-repeat' alt="banner" />
+                    data-thumb='{{!empty($banner['image'])}}'>
+                    <img src='{{!empty($banner['image'])}}' data-bgfit='cover' data-bgrepeat='no-repeat' alt="banner" />
                     <div class="container">
                       <div class="content_slideshow">
                         <div class="row">
@@ -22,12 +24,12 @@
                               <div class='tp-caption ExtraLargeTitle sft  tp-resizeme ' data-endspeed='500'
                                 data-speed='500' data-start='1100' data-easing='Linear.easeNone' data-splitin='none'
                                 data-splitout='none' data-elementdelay='0.1' data-endelementdelay='0.1'
-                                style='z-index:2; white-space:nowrap;'><span>{{$banner['title1']}}</span> </div>
+                                style='z-index:2; white-space:nowrap;'><span>{{!empty($banner['title1'])}}</span> </div>
                               <div class='tp-caption LargeTitle sfl  tp-resizeme ' data-endspeed='500' data-speed='500'
                                 data-start='1300' data-easing='Linear.easeNone' data-splitin='none' data-splitout='none'
                                 data-elementdelay='0.1' data-endelementdelay='0.1'
                                 style='z-index:3; white-space:nowrap;'><span
-                                  style="font-weight:normal; display:block">{{$banner['title2']}}</div>
+                                  style="font-weight:normal; display:block">{{!empty($banner['title2'])}}</div>
                               <div class='tp-caption sfb  tp-resizeme ' data-endspeed='500' data-speed='500'
                                 data-start='1500' data-easing='Linear.easeNone' data-splitin='none' data-splitout='none'
                                 data-elementdelay='0.1' data-endelementdelay='0.1'
@@ -39,7 +41,7 @@
                     </div>
                   </li>
                   @endforeach
-
+                  @endif
                 </ul>
                 <div class="tp-bannertimer"></div>
               </div>
