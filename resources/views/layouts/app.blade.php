55407d3e043c380bbe6e1addf8e407da8161b1a0
rajeshrprompttech <rajesh.r@prompttechsolutions.com>
2023-05-01 16:05:33 +0530

    stock checking condition updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,7 @@
     </style>
     <script>
         $(document).bind("contextmenu", function(e) {
-            return false;
+            //  return false;
         });
     </script>
 </head>
@@ -711,43 +711,43 @@
     </script>
     <script>
         /*   $('.add-to-wishlist').click(function(e) {
-                                                                e.preventDefault();
-                                                                // $('#review_button').prop('disabled', true);
-                                                                $.ajax({
-                                                                    type: "POST",
-                                                                    url: '{{ url('wishlist-add') }}',
-                                                                    headers: {
-                                                                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                                                                    },
-                                                                    data: {
-                                                                        product_id: $(this).data('cpidw')
-                                                                    },
-                                                                    success: function(response) {
-                                                                        if (response.status == 1) {
-                                                                            $("#my_btn_heart").css({
-                                                                                'color': 'red'
-                                                                            });
-                                                                            Toastify({
-                                                                                text: "Product Added",
-                                                                                className: "info",
-                                                                                close: true,
-                                                                                style: {
-                                                                                    background: "#1cad6a",
+                                                                        e.preventDefault();
+                                                                        // $('#review_button').prop('disabled', true);
+                                                                        $.ajax({
+                                                                            type: "POST",
+                                                                            url: '{{ url('wishlist-add') }}',
+                                                                            headers: {
+                                                                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                                                            },
+                                                                            data: {
+                                                                                product_id: $(this).data('cpidw')
+                                                                            },
+                                                                            success: function(response) {
+                                                                                if (response.status == 1) {
+                                                                                    $("#my_btn_heart").css({
+                                                                                        'color': 'red'
+                                                                                    });
+                                                                                    Toastify({
+                                                                                        text: "Product Added",
+                                                                                        className: "info",
+                                                                                        close: true,
+                                                                                        style: {
+                                                                                            background: "#1cad6a",
+                                                                                        }
+                                                                                    }).showToast();
+                                                                                } else {
+                                                                                    Toastify({
+                                                                                        text: 'product already added',
+                                                                                        className: "info",
+                                                                                        close: true,
+                                                                                        style: {
+                                                                                            background: "#e11414",
+                                                                                        }
+                                                                                    }).showToast();
                                                                                 }
-                                                                            }).showToast();
-                                                                        } else {
-                                                                            Toastify({
-                                                                                text: 'product already added',
-                                                                                className: "info",
-                                                                                close: true,
-                                                                                style: {
-                                                                                    background: "#e11414",
-                                                                                }
-                                                                            }).showToast();
-                                                                        }
-                                                                    }
-                                                                });
-                                                            }); */
+                                                                            }
+                                                                        });
+                                                                    }); */
     </script>
 
     <script>
@@ -798,6 +798,7 @@
                         background: "red",
                     }
                 }).showToast();
+                return;
             }
             var setting = {
                 url: '{{ url('/add-to-cart') }}',
