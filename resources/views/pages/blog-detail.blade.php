f8d3b34fa37f9108e910930756ed60ad35b30703
PranavPrompttech <pranav.mp@prompttechsolutions.com>
2023-05-01 18:25:28 +0530

    blog designing compleated

---
+++ resources/views/pages/blog-detail.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+@section('content')
+
+
+
+<div class="container">
+    <div class="blog-details">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="blog-head">
+
+                 
+                    <div>
+                        <span  class="blgimg">
+                            <img src="https://images.unsplash.com/photo-1537589376225-5405c60a5bd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="Blog Image"  style="width: 100% ;  border-radius: 64px; ">         
+                        </span>
+                        <h3 class="blgheads">
+                           Lorem ipsum dolor sit amet.
+                        </h3>
+                         <div class="userDeta">
+                        
+                                        <div class="avimg">
+                                            <img src="https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg" alt="">
+                                        </div>
+                                        <div class="avcontent">
+                                           
+                                            <h6 class="avheadtext">
+                                                Lorem ipsum dolor sit amet.
+                                            </h6>
+                                            <p>Sep 07th 2022</p>
+                                        </div>
+                                    </div>
+                </div>
+
+                        <p style="text-align: justify">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non voluptatibus recusandae quam, illo nesciunt aliquam sed repellendus commodi culpa excepturi cum quas delectus sint dolore voluptatem optio reiciendis doloribus, quod molestias, quos quo voluptas! Iusto corrupti voluptatum magni magnam excepturi ex, dolorum sed soluta quae pariatur eligendi ratione facere placeat voluptates, officia porro error aliquid, nam vel! Quod quaerat cupiditate quos? Aliquam excepturi tempora exercitationem, culpa fugit eligendi maiores corrupti unde temporibus? Consequuntur, accusamus aperiam, doloremque porro mollitia quas ducimus distinctio perspiciatis architecto, harum vitae magnam iure? Asperiores debitis odio suscipit cum molestias nemo illo tempore eaque error fuga, repellendus illum commodi animi maxime, iusto inventore obcaecati quo! Aliquid dolores a eveniet. Maiores est esse modi veritatis accusantium eum voluptatibus officia enim dolorem sit aliquam molestias necessitatibus nihil culpa, provident perspiciatis qui ad, harum consectetur eligendi maxime ex dolorum sed! Autem corrupti ab perferendis a soluta corporis dicta ex illum? Ratione nam amet quam corrupti illo accusamus tempore aperiam laborum. Enim quibusdam, consectetur asperiores explicabo animi vitae repellat temporibus aliquid perferendis odit culpa tempore magnam soluta voluptatem consequatur debitis eius ipsum dignissimos aliquam nostrum nemo amet ut? Facilis vero magnam tempore explicabo ratione expedita, nesciunt natus iure officiis ducimus suscipit.
+                        </p>
+                    </div>
+                    
+             </div>
+            
+        </div>
+    </div>
+</div>
+
+
+
+
+
+@endsection
